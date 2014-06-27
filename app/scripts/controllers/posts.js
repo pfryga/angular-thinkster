6d0b2fb8661148d87f2eed7eba4d9680b0d89c99
Piotr Fryga <piotr.fryga@allegro.pl>
2014-06-27 11:02:06 +0200

    new functions

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -9,8 +9,19 @@
  */
 angular.module('newsApp')
   .controller('PostsCtrl', function ($scope) {
+  	$scope.posts = [];
     $scope.post = {
     	url: 'http://',
     	title: ''
+    };
+    $scope.submitPost = function () {
+    	$scope.posts.push($scope.post);
+    	$scope.post = {
+	    	url: 'http://',
+	    	title: ''
+	    };
+    };
+    $scope.deletePost = function (index) {
+    	$scope.posts.splice(index, 1);
     };
   });

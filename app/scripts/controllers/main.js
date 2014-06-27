2191f1e92a3e4504b5f615dd11009b68d9d0a281
Piotr Fryga <piotr.fryga@allegro.pl>
2014-06-27 10:03:11 +0200

    yeoman angular scaffolding

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the newsApp
+ */
+angular.module('newsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
